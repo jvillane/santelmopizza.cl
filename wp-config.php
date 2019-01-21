1241520375f17912ce9c3cdc95ac998a68746591
jvillane <jvillane@gmail.com>
2019-01-21 13:58:21 -0300

    INIT: commit inicial

--- wp-config.php
+++ wp-config.php
@@ -63,7 +63,7 @@ define('NONCE_SALT',       'EVCJ!Y#ZxC!@Y(J1OMMnNDwmXDJw|X{cV# ,Yan?AF5E;rwvGQop
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+$table_prefix  = 'wpopp_';
 
 /**
  * For developers: WordPress debugging mode.
